6c6689a2ed1da0d217f42b1da1a0264300abecf7
Chirag2717 <chirag.khurana@dcmail.ca>
2025-04-08 00:41:07 -0400

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,94 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import "./App.css";
-import IssueBook from "./components/issueBook/IssueBook";
-import TestConnectivity from "./pages/TestConnectivity";
-import StaticPage from "./pages/StaticPage";
-import Header from './components/Header';
-import SignUp from "./components/Login/SignUp";
-import Login from "./components/Login/Login";
-import Authentication from "./components/Login/Authentication";
-import ViewBooks from "./pages/ViewBooks";
-import WelcomePage from "./pages/WelcomePage";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const App = () => {
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
-function App() {
-    return (
-        <div className="app">
-            <Header/>
-                <Routes>
-                    <Route path="/test" element={<TestConnectivity/>}/>
-                    <Route path="/static" element={<StaticPage/>}/>
-                    <Route path="/" element={<WelcomePage/>}></Route>
-                    <Route path="/signup" element={<SignUp/>}></Route>
-                    <Route path="/login" element={<Login/>}></Route>
-                    <Route path="/issuebook" element={<IssueBook/>}></Route>
-                    <Route path="/authenticate" element={<Authentication/>}></Route>
-                    <Route path="/viewBooks" element={<ViewBooks/>}></Route>
-                </Routes>
-        </div>
-    );
-}
+  const handleLogin = () => {
+    axios.post("http://localhost:5000/api/login", { username, password })
+      .then(res => {
+        alert(res.data.message);
+        setLoggedIn(true);
+      })
+      .catch(err => {
+        alert("Login failed");
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/books")
+      .then(res => setBooks(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const handleAddBook = () => {
+    if (title && author) {
+      axios.post("http://localhost:5000/api/books", { title, author })
+        .then(() => {
+          setBooks([...books, { title, author }]);
+          setTitle("");
+          setAuthor("");
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  return (
+    <div>
+      <h1>📚 Cloud based Library</h1>
+
+      {!loggedIn ? (
+        <>
+          <h2>Login</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          /><br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          /><br />
+          <button onClick={handleLogin}>Login</button>
+        </>
+      ) : (
+        <>
+          <h2>Add a New Book</h2>
+          <input
+            type="text"
+            placeholder="Book Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          /><br />
+          <input
+            type="text"
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          /><br />
+          <button onClick={handleAddBook}>Add Book</button>
+
+          <h2>Books in the Library</h2>
+          <ul>
+            {books.map((book, idx) => (
+              <li key={idx}>
+                {book.title} by {book.author}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
